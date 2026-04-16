96f918dba73109c96b1140142a9a4cdc9004ab36
YURIVIEIRAMARQUESDEOUZA <yv09716@gmail.com>
2026-04-16 14:05:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const convertButtom =document.querySelector(".convert-Buttom")
+
+function convertValues(){
+    const convertValue = document.querySelector(".inputValue"). value2
+    const valor =document.querySelector(".result")
+    const valoConvertido = document.querySelector(".result2")
+
+    const dollar = 5.20
+
+    const convertedValue = convertValue / dollar
+
+    valor.innerHTML = convertValue 
+
+    console.log(convertedValue)
+}
+
+convertButtom.addEventListener ("click", convertValues)
